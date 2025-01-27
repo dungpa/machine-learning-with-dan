929a51caf1d32f65208c6671a84dac8514c3405e
Anh-Dung Phan <phananhdung309@yahoo.com>
2025-01-27 19:27:55 +0000

    Restructure

--- scripts/001-animal-sorter.py
+++ scripts/001-animal-sorter.py
@@ -1,8 +1,19 @@
 from mlforkidsimages import MLforKidsImageProject
-from tkinter import Tk, messagebox
+from tkinter import Tk, messagebox, Label
+from PIL import Image, ImageTk
 
 # treat this key like a password and keep it secret!
 key = "c0e76be0-d9b3-11ef-83aa-75c08270f6280d1b2cc1-7ed4-436a-a0f0-ee317f08e381"
+
+root = Tk()
+image_path = "alligator.jpg"
+image = Image.open(image_path)
+image = image.resize((500, 500), Image.Resampling.LANCZOS)  # Resize image to fit the window
+tk_image = ImageTk.PhotoImage(image)
+
+# Create a Label widget to display the image
+image_label = Label(root, image=tk_image)
+image_label.pack(pady=10)
 
 # this will train your model and might take a little while
 myproject = MLforKidsImageProject(key)
@@ -14,9 +25,7 @@ demo = myproject.prediction("alligator.jpg")
 label = demo["class_name"]
 confidence = demo["confidence"]
 
-root = Tk()
-root.withdraw()
-
 # CHANGE THIS to do something different with the result
 messagebox.showinfo("The results are:", "I think it is closest to the: '%s' with %d%% confidence" % (label, confidence))
 
+root.mainloop()
