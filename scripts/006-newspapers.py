4a64cca065601b4ecfe16c07298af49286bd8ad2
Anh-Dung Phan <phananhdung309@yahoo.com>
2025-03-16 14:34:58 +0000

    Add UI

--- scripts/006-newspapers.py
+++ scripts/006-newspapers.py
@@ -1,6 +1,6 @@
 import requests
 from authorisation import api_key_006
-from tkinter import messagebox, Tk
+from tkinter import messagebox, Tk, Canvas, NORMAL, HIDDEN
 
 headlines = ["REUSABLE BAGS IN ENGLAND", "BEAVERS RELEASED FROM SANCTUARY", "SAND TIGER SHARK BORN IN AQUARIUM DISCOVERY", "BLOOMING SEASON FOR FLOWERS", "FIRST ANIMAL BORN IN SEA LIFE", 
 "BUMFRESH CATASTROPHE", "BOTTOMS TURN PURPLE", "BILLIONARE IS IN FOR A DISASTER OF BUMFRESH", "DESTRUCTION OF BUMFRESH", "HORROR OF PURPLE BOTTOMS"]
@@ -21,7 +21,23 @@ def classify(text):
         response.raise_for_status()
         
 root = Tk()
-root.withdraw()
+c = Canvas(root, width=400, height=500)
+c.configure(bg='deep sky blue', highlightthickness=0)
+c.body_color = 'yellow'
+body = c.create_oval(35, 20, 365, 350, outline=c.body_color, fill=c.body_color)
+ear_left = c.create_polygon(75, 80, 75, 10, 165, 70, outline=c.body_color, fill=c.body_color)
+ear_right = c.create_polygon(255, 45, 325, 10, 320, 70, outline=c.body_color, fill=c.body_color)
+
+foot_left = c.create_oval(65, 320, 145, 360, outline=c.body_color, fill=c.body_color)
+foot_right = c.create_oval(250, 320, 330, 360, outline=c.body_color, fill=c.body_color)
+eye_left = c.create_oval(130, 110, 160, 170, outline='black', fill='white')
+pupil_left = c.create_oval(140, 145, 150, 155, outline='black', fill='black')
+eye_right = c.create_oval(230, 110, 260, 170, outline='black', fill='white')
+pupil_right = c.create_oval(240, 145, 250, 155, outline='black', fill='black')
+
+mouth_happy = c.create_line(170, 250, 200, 282, 230, 250, smooth=1, width=2, state=NORMAL)
+
+c.pack()
 
 for headline in headlines:
     # CHANGE THIS to something you want your machine learning model to classify
